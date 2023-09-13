24708704a7092e9704322a35252146c7c7b839df
BrianKvin <briankvin@gmail.com>
2023-09-13 17:22:40 +0300

    Deliverables

--- hms-api/base/settings.py
+++ hms-api/base/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed apps
+    'accounts',
+    'hms',
+    # third-party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
