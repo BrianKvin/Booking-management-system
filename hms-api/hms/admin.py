e096c88c0826b01318e4d7e570e58f3553b8a680
BrianKvin <briankvin@gmail.com>
2023-09-16 14:59:52 +0300

    edited admin.py

--- hms-api/hms/admin.py
+++ hms-api/hms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Physio, Patient, Service, Booking, Billing
 
 # Register your models here.
+admin.site.register(Physio)
+admin.site.register(Patient)
+admin.site.register(Service)
+admin.site.register(Booking)
+admin.site.register(Billing)
--- hms-api/hms/views.py
+++ hms-api/hms/views.py
@@ -52,6 +52,35 @@ class PatientView(APIView):
         serializer = PatientSerializer(patient, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # CREATE a new patient
+    def post(self, request):
+        serializer = PatientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # GET a specific patient by ID
+    def retrieve(self, request, pk):
+        patient = Patient.objects.get(pk=pk)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # UPDATE a specific patient by ID
+    def update(self, request, pk):
+        patient = Patient.objects.get(pk=pk)
+        serializer = PatientSerializer(patient, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # DELETE a specific patient by ID
+    def destroy(self, request, pk):
+        patient = Patient.objects.get(pk=pk)
+        patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class ServiceView(APIView):
     def get(self, request):
