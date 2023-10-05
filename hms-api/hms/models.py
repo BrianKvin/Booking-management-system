c3ac7eb44af47e88b47ef9ca3cf27f0d97e70d69
JI-Maina <irungujmaina@gmail.com>
2023-10-05 13:13:33 +0300

    changed payment status to False

--- hms-api/hms/models.py
+++ hms-api/hms/models.py
@@ -80,7 +80,7 @@ class Booking(models.Model):
         ('seen', 'Seen')
     )
 
-    payment = models.BooleanField(default=True)
+    payment = models.BooleanField(default=False)
     physio = models.ForeignKey(Physio, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
